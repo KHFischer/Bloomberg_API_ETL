4fc927f348387a258aa139874e1a20eb0dad9dfa
Kalle Fischer <111733603+KHFischer@users.noreply.github.com>
2022-11-16 14:27:37 +0100

    Create load.py
    Saving the transformed df to csv file

---
+++ Load.py
@@ -0,0 +1,8 @@
+def main():
+
+    print('[Load] Start')
+    print('[Load] Saving data to csv')
+    df.to_csv('Bloomberg_data.csv')
+
+    print('[Load] Data saved as "Bloomberg_data.csv"')
+    print('[Load] End.')
