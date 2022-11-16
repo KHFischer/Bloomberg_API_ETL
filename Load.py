fd9543e1eef4905b16becf372fae764532fe975e
Kalle Fischer <111733603+KHFischer@users.noreply.github.com>
2022-11-16 14:32:11 +0100

    Update Load.py
    Updated JSON option

--- Load.py
+++ Load.py
@@ -1,8 +1,18 @@
+def load_to_csv(df):
+    output = df.to_csv('Bloomberg_data.csv')
+    return output
+
+
+def load_to_json(df):
+    output = df.to_json('Bloomberg_data.json')
+    return output
+
+
 def main():
 
     print('[Load] Start')
     print('[Load] Saving data to csv')
-    df.to_csv('Bloomberg_data.csv')
+    load_to_csv(df)
 
     print('[Load] Data saved as "Bloomberg_data.csv"')
     print('[Load] End.')
